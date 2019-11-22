98c3082f206293bf0f5460c2d3183eb09b4fa0cf
Nathan Drezner <nathan@drezner.com>
2019-11-21 22:22:47 -0500

    htaccess

--- js/about.js
+++ js/about.js
@@ -1,5 +1,5 @@
 function randomRedirect(curPage){
-	var redirects = ['remain.html', 'connection.html', 'change.html', 'walk.html', 'orange.html', 'save.html']
+	var redirects = ['remain', 'connection', 'change', 'walk', 'orange', 'save']
 	var curDirect = redirects[Math.floor(Math.random()*redirects.length)]
 	while (curDirect == curPage){
 		curDirect = redirects[Math.floor(Math.random()*redirects.length)]
