33029cb02271e40d56214bb1378741f48208a5c5
Nathan Drezner <nathan@drezner.com>
2019-11-29 20:18:17 -0500

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,5 +1,5 @@
 function randomRedirect(curPage){
-	var redirects = ['secrets/remain', 'secrets/connection', 'secrets/change', 'secrets/walk', 'secrets/orange', 'secrets/save']
+	var redirects = ['remain', 'connection', 'change', 'walk', 'orange', 'save']
 	var curDirect = redirects[Math.floor(Math.random()*redirects.length)]
 	while (curDirect == curPage){
 		curDirect = redirects[Math.floor(Math.random()*redirects.length)]
